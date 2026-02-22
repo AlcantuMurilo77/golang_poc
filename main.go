ff8a2c8296cdf34ccfbf1d7d4b41bb68fa605a81
Murilo Alcantu <alcantumurilo@gmail.com>
2026-02-21 22:41:52 -0400

    Create simple databse integration for GET events

--- main.go
+++ main.go
@@ -21,7 +21,11 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events."})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -37,6 +41,12 @@ func createEvent(context *gin.Context) {
 	event.ID = 1     //FOr now a dummy value
 	event.UserID = 1 //here aswell
 
-	models.Event.Save(event)
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event."})
+		return
+	}
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 }
