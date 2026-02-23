28c395f5fadb3a2e08ecf638eee7c4ff5474d411
Murilo Alcantu <alcantumurilo@gmail.com>
2026-02-23 03:44:47 -0400

    Pretty simple basic login check will do JWT

--- models/users.go
+++ models/users.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"errors"
 	"example.com/my_go_proj/db"
 	"example.com/my_go_proj/utils"
 )
@@ -37,4 +38,24 @@ func (u *User) Save() error {
 	u.ID = id
 
 	return err
+}
+
+func (u User) ValidateCredentials() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+
+	if err != nil {
+		return err
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("credentials invalid")
+	}
+
+	return nil
 }
